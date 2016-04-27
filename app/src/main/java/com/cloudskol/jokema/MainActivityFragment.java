36deac49ac5664015e154fe001d8bfa4aa1ad523
Thamizharasu <zarub2k@gmail.com>
2016-04-27 23:07:17 +0530

    dummy commit
    dummy commit

--- app/src/main/java/com/cloudskol/jokema/MainActivityFragment.java
+++ app/src/main/java/com/cloudskol/jokema/MainActivityFragment.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * A placeholder fragment containing a simple view.
+ * A placeholder fragment containing a simple view
  */
 public class MainActivityFragment extends Fragment {
 
