b230b1d7e3c707b475811a770168d15d5258de98
Thamizharasu <zarub2k@gmail.com>
2016-05-03 06:28:48 +0530

    Fix: function test changes enabled
    Functional tests changes are enabled

--- app/src/androidTest/java/com/cloudskol/jokema/JokeAsynAndroidTest.java
+++ app/src/androidTest/java/com/cloudskol/jokema/JokeAsynAndroidTest.java
@@ -1,6 +1,6 @@
 package com.cloudskol.jokema;
 
-import android.test.AndroidTestCase;
+import android.test.ActivityInstrumentationTestCase2;
 
 import java.util.concurrent.ExecutionException;
 
@@ -9,10 +9,22 @@ import java.util.concurrent.ExecutionException;
  *
  * Functional test class for our application
  */
-public class JokeAsynAndroidTest extends AndroidTestCase {
+public class JokeAsynAndroidTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private MainActivity activity_;
+    public JokeAsynAndroidTest() {
+        super(MainActivity.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        activity_ = getActivity();
+    }
+
+    @Test
     public void testAsync() {
-        final MainActivity activity = new MainActivity();
-        final JokeAsyncTask jokeAsyncTask = new JokeAsyncTask(activity);
+        final JokeAsyncTask jokeAsyncTask = new JokeAsyncTask(activity_);
 
         try {
             String result = jokeAsyncTask.execute().get();
